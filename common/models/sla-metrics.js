b5f96f1733d64afdf2597ad3f46706395c809056
aravindatcts <aravindsekar@Aravinds-MacBook-Pro.local>
2017-12-02 13:20:12 +0530

    commit

--- common/models/sla-metrics.js
+++ common/models/sla-metrics.js
@@ -2,4 +2,30 @@
 
 module.exports = function(Slametrics) {
 
+
+ Slametrics.getLatestSlaMetrics = (month,cb) => {
+   var filter =   { 
+       where : {month_ind : month},
+       order: 'date DESC' };
+
+  console.log(month);
+
+  return Slametrics.findOne(filter).then(function(slametrics) {
+      return slametrics;
+    }).catch(function(err) {
+      console.log(err);
+    });
+};
+
+  Slametrics.remoteMethod('getLatestSlaMetrics',{
+      description: "Get the latest SLA Metrics available, Pass Month=False for daily and Month = True for Monthly data", 
+      accepts : {arg:'month',type:'Boolean'},
+      http: { 
+          path : '/getLatestSlaMetrics',
+          verb : 'get'
+      },
+      returns: {arg: 'slametrics', type:'object'}
+  });
+
+
 };
