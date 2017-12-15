fcb7f8280f6af3ace68ae841972386a5f0314cd4
Aravind <aravindatcts@gmail.com>
2017-12-15 08:01:26 +0530

    commit

--- common/models/pendreasons.js
+++ common/models/pendreasons.js
@@ -1,15 +1,28 @@
 'use strict';
+var moment = require('moment');
+
 
 module.exports = function(Pendreasons) {
   Pendreasons.getPendReasonForDateRange = (startDate, endDate, monthInd, pendReason, cb) => {
+    var nextDate;
+    if (monthInd) {
+      nextDate = moment(endDate).add(1, 'M').toDate();
+      console.log(nextDate);
+    } else {
+      nextDate = moment(endDate).add(1, 'd').toDate();
+    }
     var filter =   {
       where:
       {'and':
-      [{'month_ind': monthInd},
-              {'date': {between: [startDate, endDate]}},
-              {'_pendreasons.reason': pendReason},
+      [
+        {'pendDate': {between: [startDate, nextDate]}},
+        {'_pendreasons.reason': pendReason},
       ]},
+      order: 'date DESC',
     };
+//
+    console.log(startDate);
+    console.log(endDate);
 
     return Pendreasons.find(filter).then(function(pendReason) {
       return pendReason;
